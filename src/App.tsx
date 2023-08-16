5af5fe029f83c55919d315fc74a782a038b8d043
Eric Le Donge <eric.ledonge@gmail.com>
2023-08-15 21:16:42 -0400

    :tada:

---
+++ src/App.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+type Task = {
+  id: number;
+  name: string;
+  done: boolean;
+};
+
+function App() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAddTask = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newTask = {
+      id: tasks.length + 1,
+      name: event.currentTarget["new-task"].value,
+      done: false,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleCompleteTask = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const id = parseInt(event.currentTarget.id.split("-")[1]);
+    const newTasks = tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  return (
+    <div style={{ height: "100%", width: "100%", backgroundColor: "black" }}>
+      <h1>TODO LIST APP</h1>
+
+      <form onSubmit={handleAddTask}>
+        <label htmlFor="new-task">Nouvelle tâche:</label>
+        <input type="text" id="new-task" name="new-task" />
+        <button type="submit">Ajouter</button>
+      </form>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input
+              type="checkbox"
+              id={`task-${task.id}`}
+              name={`task-${task.id}`}
+              checked={task.done}
+              onChange={handleCompleteTask}
+            />
+            <label
+              htmlFor={`task-${task.id}`}
+              className={task.done ? "done" : ""}
+            >
+              {task.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
